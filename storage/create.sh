5b3d301f1c890ec5f2f1c349efb79b5e62f7a5a8
Christian Woehrle <christian.woehrle@googlemail.com>
2018-10-27 10:57:32 +0200

    creating a pvc is optional to avoid the charges

--- storage/create.sh
+++ storage/create.sh
@@ -4,8 +4,13 @@ kubectl apply -f storageclass.yml
 echo "create storageclass cheap"
 kubectl apply -f storageclass_cheap.yml 
 
-echo "create persistentvolumeclaim myclaim"
-kubectl apply -f pvc.yml 
+echo "to create a pvc, execute kubectl apply -f pvc.yml "
+echo "Don't forget to delete it afterwards"
+#echo "create persistentvolumeclaim myclaim"
+#kubectl apply -f pvc.yml 
 
-echo "create pod with persitant volume"
-kubectl apply -f pod.yml 
+echo "To attach the pvc to a pod, execute kubectl apply -f pod.yml"
+echo "Don't forget to delete the pod if you want to delete the pvc"
+
+#echo "create pod with persitant volume"
+#kubectl apply -f pod.yml 
